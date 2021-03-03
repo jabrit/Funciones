be31e42f32eb47e7f21add3bccbded844ca6448f
Jorge Alberto Brito Sánchez <79551345+jabrit@users.noreply.github.com>
2021-03-03 01:08:47 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x,y,suma;
+cout<< "Ingrese el primer numero enetero\n";
+cin>> x;
+cout<< "Ingrese el segundo numero entero\n";
+cin>> y;
+suma = x+y;
+cout<< "El resutado de la suma es:\n";
+cout<< suma;
+cout<< endl;
+return 0;
+}
